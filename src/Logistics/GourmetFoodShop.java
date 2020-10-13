12d4a6819b55e4aee2f14298f77e07fe02e64f21
Poketrumpeter <gregorio@fefland.com>
2020-10-12 20:47:38 -0600

    MAde changes to shop class and tested how objects will talk to each other

--- src/Logistics/GourmetFoodShop.java
+++ src/Logistics/GourmetFoodShop.java
@@ -1,15 +1,29 @@
 package Logistics;
 
-import Food.Roll;
+import Food.*;
+import sun.jvm.hotspot.oops.HeapPrinter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GourmetFoodShop implements Store{
 
     ArrayList<Order> dailyOrders;
-    ArrayList<Roll> stock;
+    Map<String, Integer>stock;
     int dayNumber;
     String day;
+
+    public GourmetFoodShop() {
+        this.stock = new HashMap<>();
+        this.stock.put(new jellyRoll().getType(), 30);
+        this.stock.put(new pastryRoll().getType(), 30);
+        this.stock.put(new eggRoll().getType(), 30);
+        this.stock.put(new springRoll().getType(), 30);
+        this.stock.put(new SausageRoll().getType(), 30);
+        this.dayNumber = 1;
+        this.day = "01";
+    }
 
     @Override
     public void open() {
@@ -24,5 +38,28 @@ public class GourmetFoodShop implements Store{
     @Override
     public void respondToOrder(Order newOrder) {
 
+        /*
+        method to check stock to make sure we could fulfill the order, If true, respond to Customer
+        to let them buy the rolls and so we can record the order
+
+        If false, allow them to use their respond to roll outage function.
+         */
+        boolean stockAvailible = false;
+        for(Roll currentRoll: newOrder.getRolls()){
+            //want to get stock of current roll type
+            if(this.stock.get(currentRoll.getType()) != 0){
+                //Roll is in stock
+                System.out.println("In Stock");
+                this.stock.computeIfPresent(currentRoll.getType(), (k, v) -> v-1);
+            }
+        }
+        System.out.println();
+        displayStock();
+    }
+
+    public void displayStock(){
+        for(Map.Entry<String, Integer> entry: this.stock.entrySet()){
+            System.out.println("Stock for " + entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
