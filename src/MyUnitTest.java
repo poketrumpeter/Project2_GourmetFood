23c9066faeeec3fb589a26ca5896e1c0cc4e30da
Jackson Meyer <jackson.meyer21@gmail.com>
2020-10-14 12:41:24 -0400

    adding template for 10 test cases, still need to be specified

---
+++ src/MyUnitTest.java
@@ -0,0 +1,103 @@
+package org.junit;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import Logistics.GourmetFoodShop;
+import Food.Roll;
+
+
+
+public class MyUnitTest {
+
+    @Test
+    public void testStock() {
+        GourmetFoodShop testStore = new GourmetFoodShop();
+
+        //ArrayList<Roll> result = testStore.getStock();
+
+	//expectedResults = 
+
+        //assertEquals(0, result);
+
+    }
+    @Test
+    public void test2() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test3() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test4() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test5() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test6() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test7() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test8() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test9() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+    @Test
+    public void test10() {
+        //MyUnit myUnit = new MyUnit();
+
+        //String result = myUnit.concatenate("one", "two");
+
+        //assertEquals("onetwo", result);
+
+    }
+}
