4a8e125f458301b34fac58417b3f24945338efc5
Jackson Meyer <jackson.meyer21@gmail.com>
2020-10-14 16:59:02 -0400

    adding third test, need 7 more

--- src/MyUnitTest.java
+++ src/MyUnitTest.java
@@ -33,7 +33,35 @@ System.out.println("The Day Number pretest has succeeded.");
 System.out.println("The Store Open test has succeeded.");
     }
     @Test
-    public void pretestDayNumber3(GourmetFoodShop testStore) {
+    public void posttestDayNumber(GourmetFoodShop testStore) {
+        int result = testStore.dayNumber;
+
+        int expectedResult = 31;
+
+        try {
+            Assert.assertEquals(result, expectedResult);
+        } catch (AssertionError e) {    
+            System.out.println("The Day Number posttest has failed!");
+            throw e;
+}
+System.out.println("The Day Number posttest has succeeded.");
+    }
+    @Test
+    public void testCasCustomer(GourmetFoodShop testStore) {
+	CasualCustomer casCust = new CasualCustomer(1);
+	Order testOrder = new Order();
+	casCust.orderItems(testOrder);
+
+        try {
+            Assert.assertArrayEquals(result, expectedResult);
+        } catch (AssertionError e) {    
+            System.out.println("The Day Number pretest has failed!");
+            throw e;
+}
+System.out.println("The Day Number pretest has succeeded.");
+    }
+    @Test
+    public void testBusCustomer(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
@@ -47,7 +75,7 @@ System.out.println("The Store Open test has succeeded.");
 System.out.println("The Day Number pretest has succeeded.");
     }
     @Test
-    public void pretestDayNumber4(GourmetFoodShop testStore) {
+    public void testCatCustomer(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
@@ -61,7 +89,7 @@ System.out.println("The Day Number pretest has succeeded.");
 System.out.println("The Day Number pretest has succeeded.");
     }
     @Test
-    public void pretestDayNumber5(GourmetFoodShop testStore) {
+    public void testOrder(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
@@ -75,7 +103,7 @@ System.out.println("The Day Number pretest has succeeded.");
 System.out.println("The Day Number pretest has succeeded.");
     }
     @Test
-    public void pretestDayNumber6(GourmetFoodShop testStore) {
+    public void testSpringRoll(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
@@ -89,7 +117,7 @@ System.out.println("The Day Number pretest has succeeded.");
 System.out.println("The Day Number pretest has succeeded.");
     }
     @Test
-    public void pretestDayNumber7(GourmetFoodShop testStore) {
+    public void testSausageToppings(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
@@ -103,35 +131,7 @@ System.out.println("The Day Number pretest has succeeded.");
 System.out.println("The Day Number pretest has succeeded.");
     }
     @Test
-    public void pretestDayNumber8(GourmetFoodShop testStore) {
-        int result = testStore.dayNumber;
-
-        int expectedResult = 1;
-
-        try {
-            Assert.assertEquals(result, expectedResult);
-        } catch (AssertionError e) {    
-            System.out.println("The Day Number pretest has failed!");
-            throw e;
-}
-System.out.println("The Day Number pretest has succeeded.");
-    }
-    @Test
-    public void pretestDayNumber9(GourmetFoodShop testStore) {
-        int result = testStore.dayNumber;
-
-        int expectedResult = 1;
-
-        try {
-            Assert.assertEquals(result, expectedResult);
-        } catch (AssertionError e) {    
-            System.out.println("The Day Number pretest has failed!");
-            throw e;
-}
-System.out.println("The Day Number pretest has succeeded.");
-    }
-    @Test
-    public void pretestDayNumber10(GourmetFoodShop testStore) {
+    public void testInventory(GourmetFoodShop testStore) {
         int result = testStore.dayNumber;
 
         int expectedResult = 1;
